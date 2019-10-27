5229ddd5a65106de43d841bd17a2882d7a0ff0ba
Michal Szczepaniak <mssk87@gmail.com>
2019-10-27 14:59:31 +0100

    index.ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+type A = Array<string> | null;
+
+function p2 (a: A): string {
+    // [ 1, 2, 10 ] => [ '(1) (2) (3)' ]
+
+    if (a) {  // jesli a jest true to wiadomo ze  nie jest null lub cos czego sie nie spodziewamy
+        return a.map( x => `(${x})` ).join(' ')
+    }
+
+    return '';
+}
+
+console.log( p2 ([ 'abc', 'x', 'def', 'y', 'Z' ] ) );
