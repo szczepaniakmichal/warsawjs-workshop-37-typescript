7ab61ec04972d1a7ad12083bab6e52347734b51c
Michal Szczepaniak <mssk87@gmail.com>
2019-10-27 15:24:21 +0100

    export declare function

--- src/p4.ts
+++ src/p4.ts
@@ -1,6 +1,8 @@
 import { j1 } from '../js/j1';
 
-console.log( j1 ( [1,2,3,4,5,6] ) );
+export declare function j1(a: Array<number>): Array<number>;
 
-console.log( j1 ( 1,2,3,4,5,6 ) );
+console.log( j1 ([1,2,3,4,5,6] ) );
+
+console.log( j1(1) );
 
